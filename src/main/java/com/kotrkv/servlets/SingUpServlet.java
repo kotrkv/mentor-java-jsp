c688286a07a75502606113f03884e0b947c6f66f
Reshetko K.V <reshetkokv@energomera.ru>
2019-08-13 15:48:44 +0300

    Using annotation
    - using @WebServlet annotation for mapping SignUpServlet on url /sing

--- src/main/java/com/kotrkv/servlets/SingUpServlet.java
+++ src/main/java/com/kotrkv/servlets/SingUpServlet.java
@@ -1,6 +1,18 @@
 package com.kotrkv.servlets;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/sign")
 public class SingUpServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>Hello from SingUpServlet</h1");
+    }
 }
